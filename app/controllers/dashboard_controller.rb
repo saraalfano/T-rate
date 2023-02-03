70bfaa65a09bc018d4b18629bdd15957a00d3ad0
Swerd99 <orlandi.andrea@outlook.com>
2023-02-03 20:15:28 +0100

    New dashboard 2

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,2 +1,9 @@
 class DashboardController < ApplicationController
+    before_action :require_login
+
+    def require_login
+        if !current_user
+            redirect_to new_user_session_path
+        end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {sessions: 'users/sessions', registrations: "users/registrations"}
 
   root 'welcome#index'
+
   match 'dashboard' => 'dashboard#dashboardtraduttore', :via => :get
   match 'dashboardpm'=> 'dashboard#dashboardpm', :via => :get
   match 'dashboardutente'=> 'dashboard#dashboardutente', :via => :get
