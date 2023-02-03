339cf6b5c44cd1eb72f478d07e7d0571198bce2e
Swerd99 <orlandi.andrea@outlook.com>
2023-02-03 14:02:26 +0100

    New dashboards 1

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
 
   root 'welcome#index'
-  match 'dashboard' => 'dashboard#dashboardutente', :via => :get
+  match 'dashboard' => 'dashboard#dashboardtraduttore', :via => :get
+  match 'dashboardpm'=> 'dashboard#dashboardpm', :via => :get
+  match 'dashboardutente'=> 'dashboard#dashboardutente', :via => :get
 
 
 end
